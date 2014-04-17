a7eaeb57eb8f78f55517cabeecfe15c4eb61da8e
Jon Weisz <jweisz@cs.columbia.edu>
2014-04-17 17:39:29 -0400

    Removed dependence on trajectory planner, and refactored to send trajectories internally instead of allowing moveit to send them for us because we want more control over the grasping itself.

--- scripts/grasp_message_robot_server.py
+++ scripts/grasp_message_robot_server.py
@@ -36,8 +36,10 @@ class GraspExecutor():
     """
 
     def __init__(self, init_planner = True, moveGroupName='StaubliArm', grasp_tran_frame_name = 'wam/bhand/approach_tran'):
-        self.grasp_listener = rospy.Subscriber("/graspit/grasps", graspit_msgs.msg.Grasp, self.process_grasp_msg)
-        self.reachability_checker = ReachabilityChecker.ReachabilityChecker()
+        self.grasp_listener = rospy.Subscriber("/graspit/grasps", graspit_msgs.msg.Grasp,
+                                               self.process_grasp_msg)
+        self.reachability_checker = ReachabilityChecker.ReachabilityChecker(
+                                                                  grasp_approach_tran_frame=grasp_tran_frame_name)
         self.graspit_status_publisher = rospy.Publisher("/graspit/status", graspit_msgs.msg.GraspStatus)
 
         self.last_grasp_time = 0
@@ -48,10 +50,16 @@ class GraspExecutor():
         self.wm = WorldManager.WorldManager()
 
         moveit_commander.roscpp_initialize(sys.argv)
-        moveit_commander.MoveGroupCommander
+
         self.group = moveit_commander.MoveGroupCommander(moveGroupName)
 
         self.grasp_tran_frame_name = grasp_tran_frame_name
+
+        self.trajectory_action_client = actionlib.SimpleActionClient('follow_trajectory',
+                                                                      control_msgs.msg.FollowJointTrajectoryAction )
+        self.support_surface = ''
+        self.planning_time = 5
+        self.preshape_ratio = .5
 
 
         #self.grasp_analyzer = grasp_analyzer.GraspAnalyzer(self.global_data)
@@ -59,6 +67,84 @@ class GraspExecutor():
             self.reload_model_list([])
 
 
+    def construct_pickup_goal(self, graspit_grasp_msg):
+        goal = moveit_msgs.msg.PickupGoal()
+        goal.target_name = graspit_grasp_msg.object_name
+        goal.group_name = self.group.get_name()
+        goal.end_effector = self.group.get_end_effector_link()
+        goal.allowed_planning_time = self.planning_time
+        goal.support_surface_name = self.support_surface
+        goal.planner_id = ""
+        goal.allow_gripper_support_collision = False
+        #for now, no path constraints
+        #goal.path_constraints = *path_constraints_;
+        goal.possible_grasps = [graspit_grasp_msg]
+        goal.planning_options.plan_only = True
+        goal.can_replan = False
+        goal.look_around = False
+        goal.replan_delay = 10.0
+        goal.planning_options.planning_scene_diff = True
+        goal.planning_options.robot_state.is_diff = True
+
+        return goal
+
+
+    def handle_grasp_callback(self, graspit_grasp_msg):
+
+
+        """
+
+        :type graspit_grasp_msg: graspit_msgs.msg.Grasp
+        """
+        moveit_grasp_msg = moveit_msgs.msg.Grasp(convert_graspit_msg(graspit_grasp_msg, self.self.grasp_tran_frame_name))
+        try:
+            self.pick_plan_client.wait_for_server(rospy.Duration(0))
+        except Exception as e:
+            rospy.logerr("ReachabilityChecker::handle_reachability_callback::Failed to reach pick action server with err: %s"%(e.message()))
+
+
+        pickup_goal = self.construct_pickup_goal(graspit_grasp_msg)
+        self.pick_plan_client.send_goal(pickup_goal)
+
+        success = self.pick_plan_client.wait_for_result(rospy.Duration(10.0))
+        result = []
+        if success:
+            result = self.pick_plan_client.get_result()
+            result = moveit_msgs.msg.PickupResult(result)
+            success = result.error_code.val == result.error_code.SUCCESS
+
+        return success, result
+
+    def run_trajectory(self, trajectory_msg):
+        if not self.trajectory_action_client.wait_for_server(rospy.Duration(5)):
+            rospy.logerr('Failed to find trajectory server')
+            return False,'Failed to find trajectory server', []
+
+        trajectory_action_goal = control_msgs.msg.FollowJointTrajectoryGoal()
+        trajectory_action_goal.trajectory = trajectory_msg
+        self.trajectory_action_client.send_goal()
+
+        if not self.trajectory_action_client.wait_for_result():
+            rospy.logerr("Failed to execute trajectory")
+            return False, "Failed to execute trajectory", []
+
+        trajectory_result = self.trajectory_action_client.get_result()
+        trajectory_result = control_msgs.msg.FollowJointTrajectoryResult(trajectory_result)
+        control_msgs.msg.FollowJointTrajectoryResult(trajectory_result)
+        """:type : control_msgs.msg.FollowJointTrajectoryResult"""
+        success = trajectory_result.SUCCESSFUL == trajectory_result.error_code
+
+    def run_pickup_trajectory(self, pickup_result, pickup_phase):
+        """
+
+        :type pickup_phase: int
+        :type pickup_result: moveit_msgs.msg.PickupResult
+        """
+
+        robot_trajectory_msg = moveit_msgs.msg.RobotTrajectory(pickup_result.trajectory_stages[pickup_phase])
+        trajectory_msg = robot_trajectory_msg.joint_trajectory
+        success, error_msg, trajectory_result = self.run_trajectory(trajectory_msg)
+        return success, error_msg, trajectory_result
 
 
 
@@ -101,7 +187,7 @@ class GraspExecutor():
 
             #Open the hand - Leaves spread angle unchanged
             if success and self.robot_running:
-                success, grasp_status_msg, positions = tp.open_barrett()
+                success, grasp_status_msg, positions = barrett_manager.open_barrett()
                 if not success:
                     grasp_status = graspit_msgs.msg.GraspStatus.ROBOTERROR
 
@@ -110,7 +196,7 @@ class GraspExecutor():
                 #Preshape the hand to the grasps' spread angle
 
                 if self.robot_running:
-                    success = tp.MoveHandSrv(1, [0,0,0, grasp_msg.pre_grasp_dof[0]])
+                    success = barrett_manager.move_hand(1, [0,0,0, grasp_msg.pre_grasp_dof[0]])
                     print 'pre-grasp'
                     #Pregrasp the object
                     if not success:
@@ -118,10 +204,7 @@ class GraspExecutor():
 
             if success:
                 #Move to the goal location and grasp object
-                moveit_grasp_msg = convert_graspit_msg.graspit_grasp_to_moveit_grasp(grasp_msg)
-                result = self.group.pick(grasp_msg.object_name, moveit_grasp_msg)
-                result = moveit_msgs.msg.MoveItErrorCodes(result)
-                success = result.val == result.SUCCESS
+                success, result = self.handle_grasp_callback(grasp_msg)
                 if not success:
                     grasp_status_msg = "MoveIt Failed to plan pick"
 
@@ -129,29 +212,47 @@ class GraspExecutor():
             if not success:
                 pdb.set_trace()
 
-            if success and self.robot_running:
-                #Close the hand completely until the motors stall or they hit
-                #the final grasp DOFS
-                success, grasp_status_msg, joint_angles = tp.move_hand([grasp_msg.final_grasp_dof[1],grasp_msg.final_grasp_dof[2], grasp_msg.final_grasp_dof[3], grasp_msg.final_grasp_dof[0]])
+            #Now run stuff on the robot
+            if self.robot_running:
+                #Move the hand to the pre grasp position
+                if success and self.robot_running:
+                    success, grasp_status_msg, trajectory = self.run_pickup_trajectory(result,0)
 
-            if success and self.robot_running:
-                #Now close the hand completely until the motors stall.
-                success, grasp_status_msg, joint_angles = tp.close_barrett()
+
+                #Preshape the hand before approach
+                if success:
+                    preapproach_shape = self.preshape_ratio *array(grasp_msg.final_grasp_dof)
+                    preapproach_shape[0] = grasp_msg.final_grasp_dof[0]
+                    success, grasp_status_msg, joint_angles = barrett_manager.move_hand(preapproach_shape)
+
+                #do approach
+                if success:
+                    success, grasp_status_msg, trajectory = self.run_pickup_trajectory(result,1)
+                if success:
+                    #Close the hand completely until the motors stall or they hit
+                    #the final grasp DOFS
+                    success, grasp_status_msg, joint_angles = barrett_manager.move_hand([grasp_msg.final_grasp_dof[1],grasp_msg.final_grasp_dof[2], grasp_msg.final_grasp_dof[3], grasp_msg.final_grasp_dof[0]])
+
+                #close fingers
+                if success:
+                    #Now close the hand completely until the motors stall.
+                    success, grasp_status_msg, joint_angles = barrett_manager.close_barrett()
+
                 if not success:
                     grasp_status = graspit_msgs.msg.GraspStatus.ROBOTERROR
 
-            #Now wait for user input on whether or not to lift the object
-            if success and self.robot_running:
-                selection = int(raw_input('Lift up (1) or not (0): '))
-                if selection == 1:
-                    print 'lift up the object'
-                    #Lift the object using the staubli's straight line path planner
-                    success = tp.lift_arm(.05, True)
-                    if not success:
-                        grasp_status = graspit_msgs.msg.GraspStatus.UNREACHABLE
-                        grasp_status_msg = "Couldn't lift object"
-                else:
-                    print 'not lift up the object'
+                #Now wait for user input on whether or not to lift the object
+                if success:
+                    selection = int(raw_input('Lift up (1) or not (0): '))
+                    if selection == 1:
+                        print 'lift up the object'
+                        #Lift the object using the staubli's straight line path planner
+                        success, grasp_status_msg, trajectory_result = self.run_pickup_trajectory(2)
+                        if not success:
+                            grasp_status = graspit_msgs.msg.GraspStatus.UNREACHABLE
+                            grasp_status_msg = "Couldn't lift object"
+                        else:
+                            print 'not lift up the object'
 
 
             #Maybe decide if it has been successfully lifted here...
