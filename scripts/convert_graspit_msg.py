7e359913aef5fd62546d18901659644bb3efeba2
Jon Weisz <jweisz@cs.columbia.edu>
2014-04-17 17:37:11 -0400

    Style fixes and added docstring

--- scripts/convert_graspit_msg.py
+++ scripts/convert_graspit_msg.py
@@ -8,35 +8,38 @@ import geometry_msgs.msg
 
 import trajectory_msgs.msg
 
-def barrett_positions_from_graspit_positions(self, positions):
-        names = ['/finger_1/prox_link','/finger_1/med_link','/finger_1/dist_link',
-                 '/finger_2/prox_link','/finger_2/med_link','/finger_2/dist_link',
-                 '/finger_3/med_link','/finger_3/dist_link']
-        prefix_str = 'wam/bhand/'
-        joint_names = [prefix_str + name for name in names]
-        joint_positions = zeros([len(names),1])
-        joint_positions[0] = positions[0]
-        joint_positions[1] = positions[1]
-        joint_positions[2] = positions[1]/3.0
-        joint_positions[3] = positions[0]
-        joint_positions[4] = positions[2]
-        joint_positions[5] = positions[2]/3.0
-        joint_positions[7] = positions[3]
-        joint_positions[8] = positions[3]/3.0
 
-        return joint_names, joint_positions
+def barrett_positions_from_graspit_positions(positions):
+    """
+    :param positions: Positions of the barrett hand in graspit's convention
+    :type positions: numpy.array
+    :returns a pair containing the joint names array and the joint positions
+    :rtype: (list[string],numpy.array)
+    """
+    names = ['/finger_1/prox_link', '/finger_1/med_link', '/finger_1/dist_link',
+             '/finger_2/prox_link', '/finger_2/med_link', '/finger_2/dist_link',
+             '/finger_3/med_link', '/finger_3/dist_link']
+    prefix_str = 'wam/bhand/'
+    joint_names = [prefix_str + name for name in names]
+    joint_positions = zeros([len(names), 1])
+    joint_positions[0] = positions[0]
+    joint_positions[1] = positions[1]
+    joint_positions[2] = positions[1]/3.0
+    joint_positions[3] = positions[0]
+    joint_positions[4] = positions[2]
+    joint_positions[5] = positions[2]/3.0
+    joint_positions[7] = positions[3]
+    joint_positions[8] = positions[3]/3.0
 
-
-def graspit_grasp_to_moveit_grasp(self, grasp_msg):
-    grasp_msg = graspit_msgs.msg.Grasp(grasp_msg)
+    return joint_names, joint_positions
     #Convert the grasp message to a transform
     grasp_tran = pm.toMatrix(pm.fromMsg(grasp_msg.final_grasp_pose))
-    grasp_tran[0:3,3] /=1000 #mm to meters
+    grasp_tran[0:3, 3] /= 1000      # mm to meters
 
     pregrasp_tran = pm.toMatrix(pm.fromMsg(grasp_msg.pre_grasp_pose))
-    pregrasp_tran[0:3,3] /=1000 #mm to meters
+    pregrasp_tran[0:3, 3] /= 1000     # mm to meters
 
-    pregrasp_dist = linalg.norm(pregrasp_tran[0:3,3] - grasp_tran[0:3,3])
+    pregrasp_dist = linalg.norm(pregrasp_tran[0:3, 3] - grasp_tran[0:3, 3])
 
     grasp = moveit_msgs.msg.Grasp()
     grasp.allowed_touch_objects = False
@@ -50,13 +53,13 @@ def graspit_grasp_to_moveit_grasp(self, grasp_msg):
     joint_names, goal_point.positions = self.barrett_positions_from_graspit_positions(graspit_msgs.msg.Grasp.pre_grasp_dof)
     grasp.grasp_posture.points.append(goal_point)
 
-    grasp.pre_grasp_approach.direction.vector = geometry_msgs.msg.Vector3(0,0,1)
-    grasp.pre_grasp_approach.direction.header.frame_id = self.grasp_tran_frame_name
+    grasp.pre_grasp_approach.direction.vector = geometry_msgs.msg.Vector3(0, 0, 1)
+    grasp.pre_grasp_approach.direction.header.frame_id = grasp_tran_frame_name
     grasp.pre_grasp_approach.desired_distance = pregrasp_dist
     grasp.pre_grasp_approach.min_distance = pregrasp_dist
 
     grasp.post_grasp_retreat.min_distance = .05
     grasp.post_grasp_retreat.desired_distance = .05
     grasp.post_grasp_retreat.direction.header.frame_id = '/world'
-    grasp.post_grasp_retreat.direction.vector = geometry_msgs.msg.Vector3(0,0,1)
+    grasp.post_grasp_retreat.direction.vector = geometry_msgs.msg.Vector3(0, 0, 1)
     return grasp
